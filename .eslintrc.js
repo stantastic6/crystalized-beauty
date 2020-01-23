6d0f7aac0b1d16a120823a2f8f4248c5f5102785
Stanley Jackson <stanjackson6@gmail.com>
2020-01-23 00:44:23 -0800

    dependencies and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-  parserOptions: {
-    sourceType: 'module',
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
   },
-  parser: 'babel-eslint',
-  env: {
-    node: true,
-  },
-  extends: ['standard', 'prettier', 'prettier/standard', 'plugin:jest/recommended'],
-  plugins: ['prettier', 'jest'],
   rules: {
-    'promise/catch-or-return': 'error',
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        semi: true,
-      },
-    ],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
   },
 };
