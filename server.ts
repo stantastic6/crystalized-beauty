ad2ec07bc95f8229ebb159664323584e892ef202
Stanley Jackson <stanjackson6@gmail.com>
2020-02-14 00:26:02 -0800

    Fix Heroku build

---
+++ server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { Application, Request, Response } from 'express';
+import path from 'path';
+
+const app: Application = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function(req: Request, res: Response) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT);
